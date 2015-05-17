43c92d4d9dc4a57b2af273ef1ef98b84a47c1caa
Nicholas Wertzberger <wertnick@gmail.com>
2015-05-17 16:09:54 -0500

    Updated with stuff from other projects.
    Added the code I had made in other projects. This will change
    considerably.

---
+++ src/main/java/com/ideaheap/jade/fluent/containers/ContainerKiller.java
@@ -0,0 +1,24 @@
+package com.ideaheap.jade.fluent.containers;
+
+import jade.core.Agent;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.StaleProxyException;
+
+/**
+ * Created by nwertzberger on 5/17/15.
+ */
+public class ContainerKiller {
+
+    public static void killContainerOf(Agent agent) {
+        final AgentContainer containerController = agent.getContainerController();
+        agent.doDelete();
+        new Thread(() -> {
+            try {
+                containerController.kill();
+            } catch (StaleProxyException e) {
+                // We're going to eat this. I hope you didn't care about this.
+                //logger.error("Had exception on killContainerOf", e);
+            }
+        }).start();
+    }
+}
---
+++ src/main/java/com/ideaheap/jade/fluent/messages/IntegerMessageContentReceiver.java
@@ -0,0 +1,8 @@
+package com.ideaheap.jade.fluent.messages;
+
+/**
+ * Created by nwertzberger on 5/17/15.
+ */
+public interface IntegerMessageContentReceiver {
+    void onMessage(Integer integer);
+}
